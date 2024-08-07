acc2e2d0c52fdfc477adc144cbf7be710d1db882
AVRI-FROM-HOME <AVRI@>
2024-08-07 21:57:26 +0300

    resetGameFix

--- public/index.js
+++ public/index.js
@@ -1,58 +1,6 @@
 
 
 
-
-// const socket = io();
-// const cells = document.querySelectorAll('.cell');
-// const resetButton = document.getElementById('resetButton');
-
-
-// cells.forEach(cell => {
-//   cell.addEventListener('click', () => {
-//     const index = cell.getAttribute('data-index');
-//     socket.emit('makeMove', index);
-//   });
-// });
-
-// resetButton.addEventListener('click', () => {
-//   socket.emit('resetGame');
-// });
-
-// socket.on('gameState', ({ gameState }) => {
-
-//   cells.forEach((cell, index) => { cell.textContent = gameState[index] });
-//   const winner = checkWin(gameState);
-
-
-//   if (winner) {
-//     const displayMsg = winner === winner === 'Draw' ? "No one won !!" : `${winner} wins !`;
-//     Swal.fire({
-
-// })
-//   }
-// });
-
-// const checkWin = (board) => {
-
-//   const winPatterns = [
-//     [0, 1, 2], [3, 4, 5],
-//     [6, 7, 8], [0, 3, 6],
-//     [1, 4, 7], [2, 5, 8],
-//     [0, 4, 8], [2, 4, 6],
-//   ];
-
-
-//   for (let pattern of winPatterns) {
-//     const [a, b, c] = pattern;
-//     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-//       return board[a];
-//     }
-//   };
-//   return board.includes(null) ? null : 'Draw';
-// };
-
-
-
 const socket = io();
 const cells = document.querySelectorAll('.cell');
 const bodyHtml = document.getElementById('bodyHtml');
@@ -78,7 +26,6 @@ socket.on('gameState', async ({ gameState }) => {
 
   if (winner) {
 
-
     bodyHtml.style.display = 'none';
     const displayMsg = winner === 'Draw' ? "No one won !" : `${winner} wins !`;
 
@@ -90,7 +37,8 @@ socket.on('gameState', async ({ gameState }) => {
       confirmButtonText: 'OK'
     });
 
-     bodyHtml.style.display = 'block';
+    socket.emit('resetGame');
+    bodyHtml.style.display = 'block';
   }
 
 
